3cb1fc36a49dc257e635070ea406b9c419c938dc
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-01-04 13:00:18 -0400

    design of newNote ready

---
+++ src/NewNote.tsx
@@ -0,0 +1,12 @@
+import NoteForm from "./components/NoteForm";
+
+const NewNote = () => {
+  return (
+    <div>
+      <h1 className="mb-4">new Note</h1>
+      <NoteForm />
+    </div>
+  );
+};
+
+export default NewNote;
