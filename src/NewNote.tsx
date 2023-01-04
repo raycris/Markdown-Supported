29721859acc610b24fd899a827e58e4306fd327b
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-01-04 16:29:48 -0400

    note creation logic done

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,10 +1,15 @@
+import { NoteData } from "./App";
 import NoteForm from "./components/NoteForm";
 
-const NewNote = () => {
+type NewNoteProps = {
+  onSubmit: (data: NoteData) => void;
+};
+
+const NewNote = ({ onSubmit }: NewNoteProps) => {
   return (
     <div>
       <h1 className="mb-4">new Note</h1>
-      <NoteForm />
+      <NoteForm onSubmit={onSubmit} />
     </div>
   );
 };
